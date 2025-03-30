fdd814320248515625adb4db1fe70c529758f2c4
Widyantoalif <alifwidiyanto46@gmail.com>
2025-03-30 23:10:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var nama = prompt("Halo, selamat lebaran ya. Nama kamu siapa?");
+alert("Oke " + nama + ", maafin ya kalo ada salah");
+alert("Salah kata");
+alert("Salah perbuatan");
+alert("Salah transfer");
+alert("Ataupun salah menaruh rasa kepada " + nama + " 🤗");
+alert("Boleh dong THR nya, isiin gopay :\n08123456789");
